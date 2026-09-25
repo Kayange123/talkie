ea5a0a6a50b206a7594ffeae3ef80e29cc9ad268
Ayubu Kayange <kayangejr3@gmail.com>
2026-09-25 08:40:24 +0300

    style(auth): add gradient background to auth pages

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,7 +2,9 @@ import React from "react";
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-screen flex items-center justify-center">{children}</div>
+    <div className="flex-center min-h-screen w-full bg-gradient-to-br from-dark-2 via-dark-1 to-dark-4 px-4 py-10">
+      {children}
+    </div>
   );
 };
 
